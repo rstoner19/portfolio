edaa1a7cd6eb7eb49e13af66b756f8e9dfb70cac
Rick Stoner <stonerrick19@gmail.com>
2016-02-05 13:46:31 -0800

    added other CSS files to organize it better, implemented handlebar and other minor tweaks

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,7 +5,7 @@ var portData = [
     title: 'Campfire Coffee',
     projectUrl: 'http://rstoner19.github.io/campfire-coffee',
     category: '201 CodeFellows',
-    createdOn: '2015-01-15',
+    createdOn: '2016-01-15',
     detail: 'A project in CodeFellows 201 that required an output based on a random value based on a minimum and maximum input, as well as allowing the user to input data for a new coffe location.'
   },
   {
@@ -20,7 +20,7 @@ var portData = [
     projectUrl: 'http://pasaunders.github.io/cocktail-project/index.html',
     category: '201 CodeFellows',
     image: 'img/cliquor.jpg',
-    createdOn: '2015-01-29',
+    createdOn: '2016-01-29',
     detail: 'A group project where we created a list of drinks based on a user\'s ingredients that are stored on local storage.  If the user does put in a drink, it\'ll be added to a database.  Once the user has their ingredients in, they can display drinks in the databased that are sorted based on what percentage of ingredients in the drink they have.  Additionally, it can be sorted by based liqour or various categories.'
   }
 ];

c78bcddfe2104714757aaf44e16963b9f24b0ad7
Rick Stoner <stonerrick19@gmail.com>
2016-02-17 15:12:59 -0800

    altered call to pull data from server

--- js/gitRepo.js
+++ js/gitRepo.js
@@ -4,16 +4,21 @@
   repos.all = [];
 
   repos.requestRepos = function(callback){
-    $.ajax({
-      url: 'https://api.github.com/users/rstoner19/repos' +
-      			'?per_page=10&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        return repos.all = data;
-      }
-    })
-    .done(callback);
+    $.get('/github/users/rstoner19/repos' + '?per_page=10' + '&sort=updated'
+  ).done(function(data, message, xhr){
+    return repos.all = data;
+  })
+  .done(callback);
+    // $.ajax({
+    //   url: 'https://api.github.com/users/rstoner19/repos' +
+    //   			'?per_page=10&sort=updated',
+    //   type: 'GET',
+    //   headers: { 'Authorization': 'token ' + githubToken },
+    //   success: function(data, message, xhr) {
+    //     return repos.all = data;
+    //   }
+    // })
+    // .done(callback);
   };
 
   module.repos = repos;

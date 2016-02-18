dde81fa015389e9e939082f1e9cb28bce41f90c8
Rick Stoner <stonerrick19@gmail.com>
2016-02-17 18:29:03 -0800

    deleted old code no longer needed

--- js/gitRepo.js
+++ js/gitRepo.js
@@ -9,16 +9,6 @@
     return repos.all = data;
   })
   .done(callback);
-    // $.ajax({
-    //   url: 'https://api.github.com/users/rstoner19/repos' +
-    //   			'?per_page=10&sort=updated',
-    //   type: 'GET',
-    //   headers: { 'Authorization': 'token ' + githubToken },
-    //   success: function(data, message, xhr) {
-    //     return repos.all = data;
-    //   }
-    // })
-    // .done(callback);
   };
 
   module.repos = repos;
